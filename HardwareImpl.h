d47775814e4f2de42021075cc0127dd27879ff17
fulva1230 <b10503137@gapps.ntust.edu.tw>
2020-08-26 16:34:16 +0800

    init commit

---
+++ HardwareImpl.h
@@ -0,0 +1,61 @@
+//
+// Created by fulva on 2020/8/25.
+//
+
+#ifndef F446ZE_HARDWAREIMPL_H
+#define F446ZE_HARDWAREIMPL_H
+
+#include <mbed.h>
+#include <vector>
+#include <chrono>
+#include <USBSerial.h>
+
+//TODO change the implementation to adapt some protocols
+USBSerial bufferedSerial{};
+constexpr int BUFFER_SIZE = 20;
+
+class Hardware {
+private:
+    Timer timer;
+    char buffer[BUFFER_SIZE];
+    int bufferBoundary{0};
+    int readindex{0};
+public:
+    explicit Hardware() {
+
+    }
+
+    // any initialization code necessary to use the serial port
+    void init() {
+        bufferedSerial.init();
+        bufferedSerial.set_blocking(false);
+        timer.start();
+    }
+
+    // read a byte from the serial port. -1 = failure
+    int read() {
+        if (readindex >= bufferBoundary) {
+            bufferBoundary = bufferedSerial.read(&buffer, BUFFER_SIZE);
+            if (bufferBoundary > 0) {
+                readindex = 0;
+            } else {
+                return -1;
+            }
+        }
+        int returnByte = buffer[readindex];
+        ++readindex;
+        return returnByte;
+    }
+
+    // write data to the connection to ROS
+    void write(uint8_t *data, int length) {
+        bufferedSerial.write(data, length);
+    }
+
+    // returns milliseconds since start of program
+    unsigned long time() {
+        return timer.elapsed_time().count() / 1000;
+    }
+};
+
+#endif //F446ZE_HARDWAREIMPL_H
---
+++ main.cpp
@@ -0,0 +1,46 @@
+#include <ros/node_handle.h>
+#include <std_msgs/String.h>
+#include <std_msgs/Float64.h>
+#include "HardwareImpl.h"
+#include <mbed.h>
+
+std_msgs::Float64 local_time_msg;
+ros::Publisher micontroller_time_pub("micontime", &local_time_msg);
+ros::NodeHandle_<Hardware> nh;
+
+void rosSpin() {
+    while (true) {
+        nh.spinOnce();
+        ThisThread::sleep_for(100ms);
+    }
+}
+
+//int main() {
+//    Thread rosSpinThread;
+//    DigitalOut led(LED1);
+//    nh.initNode();
+//    rosSpinThread.start(&rosSpin);
+//    nh.advertise(micontroller_time_pub);
+//    while (true) {
+//        led = !led;
+//        local_time_msg.data = nh.getHardware()->time();
+//        micontroller_time_pub.publish(&local_time_msg);
+//        ThisThread::sleep_for(20ms);
+//    }
+//    return 0;
+//}
+
+//test code
+constexpr int BUFFER_SIZET = 10;
+char buffer[BUFFER_SIZET];
+
+int main(){
+    USBSerial usbserial{};
+    usbserial.init();
+    while(true){
+        int readAmount = usbserial.read(&buffer, BUFFER_SIZET);
+        if(readAmount > 0) usbserial.write(&buffer, readAmount);
+    }
+}
+
+
