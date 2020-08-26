dfa15aa3aff35207aef531f197dfdad0d89472be
fulva1230 <b10503137@gapps.ntust.edu.tw>
2020-08-26 17:41:08 +0800

    return to usbcdc

--- main.cpp
+++ main.cpp
@@ -15,32 +15,21 @@ void rosSpin() {
     }
 }
 
-//int main() {
-//    Thread rosSpinThread;
-//    DigitalOut led(LED1);
-//    nh.initNode();
-//    rosSpinThread.start(&rosSpin);
-//    nh.advertise(micontroller_time_pub);
-//    while (true) {
-//        led = !led;
-//        local_time_msg.data = nh.getHardware()->time();
-//        micontroller_time_pub.publish(&local_time_msg);
-//        ThisThread::sleep_for(20ms);
-//    }
-//    return 0;
-//}
-
-//test code
-constexpr int BUFFER_SIZET = 10;
-char buffer[BUFFER_SIZET];
-
-int main(){
-    USBSerial usbserial{};
-    usbserial.init();
-    while(true){
-        int readAmount = usbserial.read(&buffer, BUFFER_SIZET);
-        if(readAmount > 0) usbserial.write(&buffer, readAmount);
+int main() {
+    Thread rosSpinThread;
+    DigitalOut led(LED1);
+    nh.initNode();
+    rosSpinThread.start(&rosSpin);
+    nh.advertise(micontroller_time_pub);
+    while (true) {
+        led = !led;
+        local_time_msg.data = nh.getHardware()->time();
+        micontroller_time_pub.publish(&local_time_msg);
+        ThisThread::sleep_for(20ms);
     }
+    return 0;
 }
+
+
 
 
